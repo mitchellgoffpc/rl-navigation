b0601d665f4fe3e3fe21c3486d46bb6a41d34aed
Mitchell Goff <mitchellgoffpc@gmail.com>
2023-07-05 00:18:30 -0700

    Hindsight Experience Replay for bitflip env

--- bitflip/train.py
+++ bitflip/train.py
@@ -12,15 +12,19 @@ from bitflip.environment import BitflipEnvironment
 
 @dataclass
 class BitflipConfig(BaseConfig):
-  lr = 0.0003
-  batch_size = 16
-  bit_length = 4
-  hidden_size = 128
-  batch_size = 512
-  num_episodes = 20000
-  max_episode_length = 12
-  replay_buffer_size = 1000
-  report_interval = 100
+  lr: float = 0.0003
+  bit_length: int = 32
+  hidden_size: int = 128
+  batch_size: int = 512
+  num_episodes: int = 10000
+  replay_buffer_size: int = 2000
+  goal_replacement_prob: float = 0.8
+  num_train_steps: int = 20
+  report_interval: int = 100
+
+  @property
+  def max_episode_length(self):
+    return int(self.bit_length * 1.5)
 
 
 def train(config):
@@ -32,17 +36,16 @@ def train(config):
   optimizer = torch.optim.Adam(agent.parameters(), lr=config.lr)
 
   for episode_counter in range(1, config.num_episodes + 1):
-    epsilon = max(0.00, 1. - 3 * float(episode_counter) / config.num_episodes)
+    epsilon = max(0.00, 1. - 2 * float(episode_counter) / config.num_episodes)
 
     # Roll out an episode
     state, goal = env.reset()
     for step in range(config.max_episode_length):
-      with torch.no_grad():
-        distances = agent(state[None], goal[None])
-
       # Epsilon-greedy exploration strategy
       if np.random.uniform() > epsilon:
-        action = torch.argmin(distances).numpy()
+        with torch.no_grad():
+          distances = agent(state[None], goal[None])
+          action = torch.argmin(distances).numpy()
       else:
         action = np.random.randint(0, config.bit_length)
 
@@ -56,18 +59,24 @@ def train(config):
 
     # Train the agent
     if replay.num_steps() > config.batch_size:
-      states, goals, actions, next_states, finished, _ = (x.squeeze() for x in replay.sample(config.batch_size))
+      for _ in range(config.num_train_steps):
+        states, goals, actions, next_states, finished, mask = replay.sample(config.batch_size, config.max_episode_length)
+        use_alt_goals = np.random.uniform(size=config.batch_size) < config.goal_replacement_prob
+        alt_goal_idxs = np.random.randint(0, np.sum(mask, axis=-1))
+        goals = np.where(use_alt_goals[:,None], states[np.arange(len(states)), alt_goal_idxs], goals[:,0])
+        finished = np.where(use_alt_goals, alt_goal_idxs == 0, finished[:,0])
+        states, actions, next_states = (x[:,0] for x in (states, actions, next_states))
 
-      with torch.no_grad():
-        best_future_distances = torch.clip(agent(next_states, goals).min(dim=-1).values * ~finished, 0, config.bit_length)
-      distances = agent(states, goals)[torch.arange(len(actions)), actions]
-      loss = F.smooth_l1_loss(distances, best_future_distances + 1)
-      loss.backward()
+        with torch.no_grad():
+          best_future_distances = torch.clip(agent(next_states, goals).min(dim=-1).values * ~finished, 0, config.max_episode_length)
+        distances = agent(states, goals)[torch.arange(len(actions)), actions]
+        loss = F.smooth_l1_loss(distances, best_future_distances + 1)
+        loss.backward()
 
-      optimizer.step()
-      optimizer.zero_grad()
+        optimizer.step()
+        optimizer.zero_grad()
 
-    metrics.add({"Wins": done, "Episode Length": step, "Epsilon": epsilon})
+    metrics.add({"Wins": done, "Episode Length": step+1, "Epsilon": epsilon})
 
 
 # ENTRY POINT
