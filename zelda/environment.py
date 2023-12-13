eeb056e6542bb1ec5fef3c41db2d555cfdc8580f
mitchellgoffpc <mitchellgoffpc@gmail.com>
2023-12-12 22:02:39 -0800

    Added eval.py

--- zelda/environment.py
+++ zelda/environment.py
@@ -1,43 +1,49 @@
+import gym
 import numpy as np
 from pathlib import Path
-from gym.spaces import Box
 from common.environments.nes import NESEnvironment
 
+gym.register('zelda', lambda *args, **kwargs: ZeldaEnvironment(*args, **kwargs))
+
 class ZeldaEnvironment(NESEnvironment):
+  ACTIONS = [NESEnvironment.UP, NESEnvironment.DOWN, NESEnvironment.LEFT, NESEnvironment.RIGHT]
+
   def __init__(self):
     super().__init__(Path(__file__).parent / f"zelda.nes")
     super().reset()
     self.wait(40)
-    self.step(self.START, wait=20)
-    self.step(self.START, wait=10)
-    self.step(self.A, wait=10)
+    super().step(self.START, wait=20)
+    super().step(self.START, wait=10)
+    super().step(self.A, wait=10)
     for _ in range(3):
-      self.step(self.SELECT, wait=1)
-    self.step(self.START, wait=20)
-    self.step(self.START, wait=120)
+      super().step(self.SELECT, wait=1)
+    super().step(self.START, wait=20)
+    super().step(self.START, wait=120)
     self._backup()
 
   @property
   def action_space(self):
-    return Discrete(4)
+    return gym.spaces.Discrete(4)
 
   @property
   def observation_space(self):
-    return Box(low=0, high=255, shape=(240, 256, 3), dtype=np.uint8)
+    return gym.spaces.Box(low=0, high=255, shape=(240, 256, 3), dtype=np.uint8)
 
-  def reset(self):
+  def reset(self, seed=None, options=None):
     self._restore()
-    return self.step(0)
+    return super().step(0), self.get_info()
 
-  def step(self, *args, **kwargs):
-    obs = super().step(*args, **kwargs)
-    info = {'screen_pos': self.screen_pos, 'map_pos': self.map_pos, 'pos': (*self.screen_pos, *self.map_pos)}
-    return obs, info
+  def step(self, action, *args, **kwargs):
+    obs = super().step(self.ACTIONS[action], *args, **kwargs)
+    return obs, 0, False, False, self.get_info()
 
   def pos_matches(self, a, b):
     ax, ay, amx, amy, aml = map(int, a)
     bx, by, bmx, bmy, bml = map(int, b)
     return abs(ax - bx) <= 4 and abs(ay - by) <= 4 and (amx, amy, aml) == (bmx, bmy, bml)
+
+  def get_info(self):
+    return {'screen_pos': self.screen_pos, 'map_pos': self.map_pos, 'pos': (*self.screen_pos, *self.map_pos)}
 
   @property
   def screen_pos(self):
---
+++ zelda/eval.py
@@ -0,0 +1,65 @@
+import gym
+import torch
+import random
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm, trange
+from pathlib import Path
+from zelda.models import ZeldaAgent
+from zelda.environment import ZeldaEnvironment
+
+BS = 8
+NUM_REPEATS = 8
+NUM_ROLLOUTS = 128
+
+parser = argparse.ArgumentParser(description='Evaluate a policy model')
+parser.add_argument('--plot', action='store_true', help='Plot goal states')
+args = parser.parse_args()
+
+env = gym.vector.make('zelda', BS)
+policy = ZeldaAgent(4).eval()
+policy.load_state_dict(torch.load(Path(__file__).parent / 'checkpoints' / 'policy.ckpt'))
+torch.set_grad_enabled(False)
+
+# Run rollouts to collect candidate goal states
+rollouts = []
+for _ in trange(NUM_ROLLOUTS // BS, desc="Generating goal states"):
+    states = []
+    positions = []
+    obs, _ = env.reset()
+    for _ in range(20):
+        action = env.action_space.sample()
+        for _ in range(NUM_REPEATS):
+            obs, _, _, _, info = env.step(action)
+        states.append(obs)
+        positions.append(info['pos'])
+    for i in range(BS):
+        rollouts.append([(state[i], pos[i]) for state, pos in zip(states, positions)])
+
+# Select 100 goal states, randomly sampled from these rollouts
+goal_states = [random.choice(rollout) for rollout in rollouts]
+
+# Plot the goal states
+if args.plot:
+    goal_positions = np.array(goal_states, dtype=int)
+    plt.scatter(goal_positions[:, 0], 255 - goal_positions[:, 1])
+    plt.show()
+
+# Run the model in a loop, one episode per goal state
+env = gym.make('zelda')
+successes = 0
+for goal, goal_pos in tqdm(goal_states, desc="Evaluating model"):
+    obs, _ = env.reset()
+    for _ in range(20):
+        action_probs = policy(obs[None], goal[None]).softmax(-1)
+        action = torch.multinomial(action_probs, 1).cpu().numpy().item()
+        for _ in range(NUM_REPEATS):
+            obs, _, _, _, info = env.step(action)
+        if env.pos_matches(info['pos'], goal_pos):
+            successes += 1
+            break
+
+# Record the percentage of rollouts which successfully reach the goal state
+success_rate = successes / len(goal_states)
+print(f"Success rate: {success_rate * 100}%")
